c85c3033bb1ae79f7cd49b1ddacbb38b8d392f0a
Daniel Cruz <daniel.cruz@4thsource.com>
2015-01-08 11:59:25 -0600

    Starting to assembly everything

--- AwakenYourSmile.Test/AppointmentUnitTest.cs
+++ AwakenYourSmile.Test/AppointmentUnitTest.cs
@@ -16,7 +16,7 @@ namespace AwakenYourSmile.Test
         }
         
         [TestMethod]
-        public void AppointmentCRUD()
+        public void SimpleCRUD()
         {
             var appointment = new Appointment
             {
@@ -50,6 +50,73 @@ namespace AwakenYourSmile.Test
             var removed = Appointment.Load(reloaded.ID);
 
             Assert.IsNull(removed);
+        }
+
+        [TestMethod]
+        public void PersistAllProperties()
+        {
+            var appointment = new Appointment
+            {
+                ID = Guid.NewGuid(),
+                FirstName = "FirstName",
+                LastName = "LastName",
+                AppointmentDate = DateTime.Now.AddDays(1),
+                PhoneNumber = "9876543210",
+                CellPhone = "0123456789",
+                Email = "email@someone.com",
+                Notes = "Some notes",
+                CreatedBy = "dcruz",
+                DateCreated = DateTime.Now
+            };
+
+            appointment.Save();
+
+            var loaded = Appointment.Load(appointment.ID);
+
+            Compare(appointment, loaded);
+
+            loaded.FirstName += "1";
+            loaded.LastName += "1";
+            loaded.AppointmentDate = DateTime.Now.AddDays(1);
+            loaded.PhoneNumber += "1";
+            loaded.CellPhone += "1";
+            loaded.Email += "1";
+            loaded.Notes += "1";
+            loaded.CreatedBy += "1";
+            loaded.DateCreated = DateTime.Now;
+            loaded.LastUpdatedBy += "1";
+            loaded.LastUpdated = DateTime.Now;
+
+            loaded.AcceptChanges();
+
+            var reloaded = Appointment.Load(loaded.ID);
+
+            Compare(loaded, reloaded);
+
+            reloaded.Delete();
+            reloaded.AcceptChanges();
+        }
+
+        private static void Compare(Appointment appointment1, Appointment appointment2)
+        {
+            if (appointment1 == null)
+                throw new ArgumentNullException("appointment1");
+
+            if (appointment2 == null)
+                throw new ArgumentNullException("appointment2");
+
+            Assert.AreEqual(appointment1.ID, appointment2.ID);
+            Assert.AreEqual(appointment1.FirstName, appointment2.FirstName);
+            Assert.AreEqual(appointment1.LastName, appointment2.LastName);
+            Assert.AreEqual(appointment1.AppointmentDate, appointment2.AppointmentDate);
+            Assert.AreEqual(appointment1.PhoneNumber, appointment2.PhoneNumber);
+            Assert.AreEqual(appointment1.CellPhone, appointment2.CellPhone);
+            Assert.AreEqual(appointment1.Email, appointment2.Email);
+            Assert.AreEqual(appointment1.Notes, appointment2.Notes);
+            Assert.AreEqual(appointment1.CreatedBy, appointment2.CreatedBy);
+            Assert.AreEqual(appointment1.DateCreated, appointment2.DateCreated);
+            Assert.AreEqual(appointment1.LastUpdatedBy, appointment2.LastUpdatedBy);
+            Assert.AreEqual(appointment1.LastUpdated, appointment2.LastUpdated);
         }
     }
 }
--- AwakenYourSmile.Web/Models/IdentityModels.cs
+++ AwakenYourSmile.Web/Models/IdentityModels.cs
@@ -20,7 +20,7 @@ namespace AwakenYourSmile.Web.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("ApplicationContextDb", throwIfV1Schema: false)
         {
         }
 
