5f662a86b759846825c1bf8b84f377d49622a592
Daniel Cruz <daniel.cruz@4thsource.com>
2015-02-09 12:47:20 -0600

    Scheduled notifications

---
+++ AwakenYourSmile.Web/App_Data/nextAppointments.xslt
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="iso-8859-1"?>
+<xsl:stylesheet version="1.0"
+                xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+                xmlns:ms="urn:schemas-microsoft-com:xslt"
+                xmlns:dt="urn:schemas-microsoft-com:datatypes">
+  <xsl:template match="/">
+    <html xmlns="http://www.w3.org/1999/xhtml">
+      <head>
+        <title>
+          Citas pendientes
+        </title>
+      </head>
+      <body>
+        Hola, Hay citas pendientes en el transcurso del día.
+
+        El equipo de Despierta tu sonrisa.
+      </body>
+    </html>
+  </xsl:template>
+</xsl:stylesheet>
--- AwakenYourSmile.Web/Global.asax.cs
+++ AwakenYourSmile.Web/Global.asax.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Rainbow.Web.PostOffice;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
+using System.Web.Caching;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -10,12 +13,61 @@ namespace AwakenYourSmile.Web
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        private static CacheItemRemovedCallback OnCacheRemove = null;
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            AddTask("Notifications", Convert.ToInt32(ConfigurationManager.AppSettings["SecondsBetweenNotifications"]));
+        }
+
+        private void AddTask(string name, int seconds)
+        {
+            OnCacheRemove = new CacheItemRemovedCallback(CacheItemRemoved);
+            HttpRuntime.Cache.Insert(name, seconds, null,
+                DateTime.Now.AddSeconds(seconds), Cache.NoSlidingExpiration,
+                CacheItemPriority.NotRemovable, OnCacheRemove);
+        }
+
+        public void CacheItemRemoved(string k, object v, CacheItemRemovedReason r)
+        {
+            // do stuff here if it matches our taskname
+            try
+            {
+                var today = DateTime.Now;
+
+                var tomorrow = DateTime.Now.Date.AddDays(1);
+
+                var appointments = Appointment.GetAppointments(today, tomorrow);
+
+                if (appointments.Count > 0)
+                {
+                    var path = Server.MapPath(ConfigurationManager.AppSettings["NextAppointments"]);
+                    var parameters = new Dictionary<string, string>();
+
+                    using (var template = System.IO.File.OpenRead(path))
+                    {
+                        var message = PostOfficeService.Create(appointments, template, parameters);
+
+                        message.IsBodyHtml = true;
+                        message.To.Add(ConfigurationManager.AppSettings["AppointmentNotificationEmail"]);
+
+                        PostOfficeService.SendMessage(message);
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+                AddTask("Notifications", Convert.ToInt32(ConfigurationManager.AppSettings["SecondsBetweenNotifications"]));
+            }
         }
     }
 }
